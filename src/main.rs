7c00267ea6ce85f9b54e0050b912d5e8cc342378
Yann Ramin <github@theatr.us>
2022-01-10 18:03:12 -0800

    Initial import

---
+++ src/main.rs
@@ -0,0 +1,72 @@
+#![no_main]
+#![no_std]
+
+use core::sync::atomic::{AtomicUsize, Ordering};
+
+use cortex_m::{
+    self,
+    delay::Delay,
+    interrupt::{free, Mutex},
+    peripheral::NVIC,
+};
+use cortex_m_rt::entry;
+
+// These lines are part of our setup for debug printing.
+use defmt_rtt as _;
+use panic_probe as _;
+
+// Import parts of this library we use. You could use this style, or perhaps import
+// less here.
+use stm32_hal2::{
+    self,
+    adc::{self, Adc, AdcDevice},
+    clocks::Clocks,
+    gpio::{Edge, Pin, Port, PinMode, OutputType, Pull},
+    low_power,
+    pac,
+    timer::{Timer, TimerInterrupt},
+};
+
+#[entry]
+fn main() -> ! {
+    // Set up ARM Cortex-M peripherals. These are common to many MCUs, including all STM32 ones.
+    let mut cp = cortex_m::Peripherals::take().unwrap();
+    // Set up peripherals specific to the microcontroller you're using.
+    let mut dp = pac::Peripherals::take().unwrap();
+
+    // This line is required to prevent the debugger from disconnecting on entering WFI.
+    // This appears to be a limitation of many STM32 families. Not required in production code,
+    // and significantly increases power consumption in low-power modes.
+    stm32_hal2::debug_workaround();
+
+    // Create an initial clock configuration that uses the MCU's internal oscillator (HSI),
+    // sets the MCU to its maximum system clock speed.
+    let clock_cfg = Clocks::default();
+
+    // Write the clock configuration to the MCU. If you wish, you can modify `clocks` above
+    // in accordance with [its docs](https://docs.rs/stm32-hal2/0.2.0/stm32_hal2/clocks/index.html),
+    // and the `clock_cfg` example.
+    clock_cfg.setup().unwrap();
+
+    loop {
+        defmt::println!("Looping!"); // A print statement using DEFMT.
+        // Enter a low power mode. The program will wake once an interrupt fires.
+        // For example, the timer and GPIO interrupt above. But we haven't unmasked
+        // their lines, so they won't work - see the `interrupts` example for that.
+        low_power::sleep_now();
+    }
+}
+
+// same panicking *behavior* as `panic-probe` but doesn't print a panic message
+// this prevents the panic message being printed *twice* when `defmt::panic` is invoked
+#[defmt::panic_handler]
+fn panic() -> ! {
+    cortex_m::asm::udf()
+}
+
+/// Terminates the application and makes `probe-run` exit with exit-code = 0
+pub fn exit() -> ! {
+    loop {
+        cortex_m::asm::bkpt();
+    }
+}
